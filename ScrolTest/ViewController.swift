08b23c770b07b3bbbf460ec48913ef1dd2c35e18
admin <admin@admins-imac.elcam.co.il>
2017-04-04 05:50:09 +0300

    Initial Commit

---
+++ ScrolTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrolTest
+//
+//  Created by admin on 8 Nisan 5777.
+//  Copyright © 5777 Matan alpha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
